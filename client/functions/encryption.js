685fb97df7cdc1604a20df16a599f333fdf513b7
SashaChackalackalBTB <schackalackal@beatthebookinc.com>
2022-12-13 18:46:36 -0600

    barely anything

--- client/functions/encryption.js
+++ client/functions/encryption.js
@@ -3,6 +3,7 @@ import CryptoJS from 'crypto-js';
 const k = "5A7134743777217A24432646294A404E635266556A586E3272357538782F413F"
 const iv = "635166546A576E5A7134743777217A25432A462D4A614E645267556B58703273"
 
+
 export const encrypt = (message, key) => {
     key = k; // obviously not this
     // Encrypt the message using the key
